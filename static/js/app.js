bb57c68cb73531b7f934a7adbaf52e9cd9c26348
poonam-ux <shaonam@gmail.com>
2021-04-06 21:03:22 -0700

    Add function

--- static/js/app.js
+++ static/js/app.js
@@ -109,3 +109,8 @@ function buildMetadata(sample) {
         buildGauge(result.wfreq);
     });
 }
+
+function optionChanged(newSample) {
+    buildCharts(newSample);
+    buildMetadata(newSample);
+}
