e228bef0cd16bd51b41972fd96a5ec97aa3cd864
poonam-ux <shaonam@gmail.com>
2021-04-06 23:34:40 -0700

    Update gaugeChart (bonus)

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,33 +1,87 @@
 function buildGauge(wfreq) {
 	console.log(wfreq);
-    
-	var gaugeData = [
-		{
-			domain: { x: [0, 1], y: [0, 1] },
-			value: wfreq,
-			title: {text: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week'},
-			type: "indicator",
-			mode: "gauge+number",
-			gauge: {
-				axis: { range: [null, 9] },
-				steps: [
-					{ range: [0, 1], color: 'rgb(248, 243, 236)' },
-					{ range: [1, 2], color: 'rgb(244, 241, 229)' },
-					{ range: [2, 3], color: 'rgb(233, 230, 202)' },
-					{ range: [3, 4], color: 'rgb(229, 231, 179)' },
-					{ range: [4, 5], color: 'rgb(213, 228, 157)' },
-					{ range: [5, 6], color: 'rgb(183, 204, 146)' },
-					{ range: [6, 7], color: 'rgb(140, 191, 136)' },
-					{ range: [7, 8], color: 'rgb(138, 187, 143)' },
-					{ range: [8, 9], color: 'rgb(133, 180, 138)' }
-				],
-			}
-		}
-	];
-	
-	var gaugeLayout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
-	
-	Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
+    var level = parseFloat(wfreq) * 20;
+	var degrees = 180 - level;
+	var radius = 0.5;
+	var radians = (degrees * Math.PI)/180;
+	let x = radius * Math.cos(radians);
+    let y = radius * Math.sin(radians);
+
+    // Path May Have to Change to Create a Better Triangle
+    let mainPath = "M-.0 -0.05 L  .0 0.05 L";
+    let pathX = String(x);
+    let space = " ";
+    let pathY = String(y);
+    let pathEnd = " Z";
+    let path = mainPath.concat(pathX, space, pathY, pathEnd);
+    console.log(path);
+    let data = [
+        {
+            type: "scatter",
+            x:[0],
+            y:[0],
+            marker: { size: 12, color: "850000" },
+            showlegend: false,
+            text: level,
+            hoverinfo: "text+name"
+        },
+        {
+            values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+            rotation: 90,
+            text:["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            textinfo: "text",
+            textposition: "inside",
+            marker: {
+                colors: [
+                    "rgba(0,105,11,.5)",
+                    "rgba(10,120,22,.5)",
+                    "rgba(14,127,0,.5)",
+                    "rgba(110,154,22,.5)",
+                    "rgba(170,202,42,.5)",
+                    "rgba(202,209,95,.5)",
+                    "rgba(210,206,145,.5)",
+                    "rgba(232,226,202,.5)",
+                    "rgba(240, 230,215,.5)",
+                    "rgba(255,255,255,0)"
+                ]
+            },
+            labels:["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            hoverinfo: "label",
+            hole: 0.5,
+            type: "pie",
+            showlegend: false
+        }
+    ]
+
+    var layout = {
+        shapes: [
+            {
+                type: "path",
+                path: path,
+                fillcolor: "850000",
+                line: {
+                    color: "850000"
+                }
+            }
+        ],
+        title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+        height: 500,
+        width: 500,
+        xaxis: {
+            zeroline:false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        }
+    }
+	Plotly.newPlot('gauge', data, layout);
 }
 
 buildGauge()
